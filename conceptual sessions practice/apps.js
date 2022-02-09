84efb8a3683a8c7d1b6d16fb256de74800fd52d5
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-09 23:21:12 +0600

    practice added

---
+++ conceptual sessions practice/apps.js
@@ -0,0 +1,16 @@
+// let allButtton = document.getElementsByClassName('button-text');
+// let prime = document.getElementById("prime")
+// console.log(prime);
+// for(const button of allButtton){
+//     button.addEventListener("click",function(e){
+//         console.log(e.target.parentNode.parentNode)
+//         console.log(button.id)
+//         console.log(e.target.classList[1])
+//         if(e.target.id == 'prime'){
+//             document.getElementById("title").style.color = 'red'
+//         }
+//         else
+//         document.getElementById("title").style.color = 'green'
+        
+//     })
+// }
